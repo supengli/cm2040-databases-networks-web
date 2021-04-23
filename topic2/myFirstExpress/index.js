ddf0f3646180f83e2dc23db885c4227579244b3f
Supeng Li <superlisohou@gmail.com>
2021-04-23 19:33:16 +0000

    My first Express web server

--- topic2/myFirstExpress/index.js
+++ topic2/myFirstExpress/index.js
@@ -1,1 +1,7 @@
 // The index.js file of your application
+const express = require("express");
+const app = express();
+const port = 8083;
+
+app.get("/", (req, res) => res.send("<h1>Hello World!</h1>"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
