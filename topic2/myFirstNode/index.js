706d9074f939cb655bca376d95b88cc6c312b697
Supeng Li <superlisohou@gmail.com>
2021-04-23 16:32:19 +0000

    Creating my first Node.js web server

--- topic2/myFirstNode/index.js
+++ topic2/myFirstNode/index.js
@@ -1,1 +1,8 @@
 // The index.js file of your application
+let http = require("http");
+http.createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World!");
+}).listen(8081, function () {
+    console.log("Node server is running...");
+});
