23fc317c56d91e3cabe3c71d792abba4ff4ee543
Supeng Li <superlisohou@gmail.com>
2021-04-24 12:09:17 +0000

    Adding routes to your web server

--- topic2/myFirstExpress/index.js
+++ topic2/myFirstExpress/index.js
@@ -4,4 +4,5 @@ const app = express();
 const port = 8083;
 
 app.get("/", (req, res) => res.send("<h1>Hello World!</h1>"));
+app.get("/about", (req, res) => res.send("<h1>This is the about page</h1>"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
