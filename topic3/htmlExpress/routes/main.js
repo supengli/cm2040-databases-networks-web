8ff5af88ba9d33ba24623482ac0b84785cc21b7b
Supeng Li <superlisohou@gmail.com>
2021-04-24 13:00:06 +0000

    Adding HTML to your Express server

--- topic3/htmlExpress/routes/main.js
+++ topic3/htmlExpress/routes/main.js
@@ -1,1 +1,9 @@
 // The main.js file of your application
+module.exports = function (app) {
+    app.get("/", function (req, res) {
+        res.render("index.html")
+    });
+    app.get("/search", function (req, res) {
+        res.render("search.html");
+    });
+}
