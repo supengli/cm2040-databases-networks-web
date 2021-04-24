082a0be6863a2d3cc741d8dfe5c2b80920ba7d44
Supeng Li <superlisohou@gmail.com>
2021-04-24 14:26:50 +0000

    Adding dynamic HTML to your Express server

--- topic3/htmlExpressDynamic/routes/main.js
+++ topic3/htmlExpressDynamic/routes/main.js
@@ -1,1 +1,6 @@
 // The main.js file of your application
+module.exports = function (app) {
+    app.get("/", function (req, res) {
+        res.render("index.html", { title: "Dynamic title" });
+    });
+}
