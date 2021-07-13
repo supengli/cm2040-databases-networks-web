6466b82f9098863e0e47f298d481f92362c20c1e
Supeng (Soho) Li <superlisohou@gmail.com>
2021-07-13 22:18:32 +0900

    update index.js to establish db connection

--- topic7/mid-term/index.js
+++ topic7/mid-term/index.js
@@ -1,8 +1,9 @@
 // The index.js file of your application
+const util = require('util');
+const mysql = require("mysql2");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const mysql = require("mysql");
 const port = 8089;
 
 const db = mysql.createConnection({
@@ -13,14 +14,14 @@ const db = mysql.createConnection({
 });
 
 // connect to database
-/*db.connect((err) => {
+db.connect((err) => {
   if (err) {
     throw err;
   }
   console.log("Connected to database");
 });
-
-global.db = db;*/
+const dbRun = util.promisify(db.query.bind(db));
+global.dbRun = dbRun;
 
 // Serve static files in Express according to https://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
