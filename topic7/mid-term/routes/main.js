b4188f92578b77da1514e1ed3254e8d44e35631e
Supeng (Soho) Li <superlisohou@gmail.com>
2021-07-14 11:47:33 +0900

    ad show page

--- topic7/mid-term/routes/main.js
+++ topic7/mid-term/routes/main.js
@@ -17,15 +17,31 @@ module.exports = function (app) {
 
     // A route for R3B
     app.post("/add", function (req, res) {
-        console.log(req.body);
         const {deviceName, ...fields}  = req.body;
-        const device = {name: deviceName, fields: JSON.stringify(fields)};
-        dbRun("INSERT INTO devicesAdded SET ?;", device).then( value => dbRun("SELECT name, fields FROM devices;")).then(value =>res.render("add.ejs", devices=value));
+        dbRun("INSERT INTO devicesAdded SET ?;", {name: deviceName, fields: JSON.stringify(fields)}).
+        then(value =>res.render("message.ejs", message={title: `${deviceName} inserted`, body: `A new ${deviceName} with has been successfully inserted!`}));
         ;
     })
 
     // A route for R4A: Show device page
-    app.get("/addPage", function (req, res) {
-        dbRun("SELECT name, fields FROM devicesAdded;").then(value =>res.render("show.ejs", devices=value));
+    app.get("/showPage", function (req, res) {
+        dbRun("SELECT name, fields FROM devices;").then(value =>res.render("show.ejs", {devices: value, selectedDevice: undefined},));        
+    });
+
+    // Return selected device data
+    app.get("/showDashboard", function (req, res) {
+        let deviceName = req.query.deviceName;
+        dbRun("SELECT id, name, fields FROM devicesAdded WHERE ?", {name: deviceName}).
+        then(value => {
+            // If not found return a message
+            if (value.length == 0) {
+                return Promise.reject(0);
+            }
+            else {
+                // Otherwise, return selcted device data
+                res.render("show.ejs", {devices: [value[0]], selectedDevice: value}) 
+            };
+        }).catch(error => res.render("message.ejs", message={title: `${deviceName} is not found`, body: `No ${deviceName} has been inserted before`}))
+        ;        
     });
 };
