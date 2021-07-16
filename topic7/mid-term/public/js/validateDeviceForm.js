f82df51785730f1bf154fe9fef1068f5fa884a7b
Supeng (Soho) Li <superlisohou@gmail.com>
2021-07-16 23:35:20 +0900

    add some commets

--- topic7/mid-term/public/js/validateDeviceForm.js
+++ topic7/mid-term/public/js/validateDeviceForm.js
@@ -1,38 +1,45 @@
+// This function is used for validate the input of form
 function validateDeviceForm() {
 
     let deviceSelect = document.getElementById("deviceName");
     let volumeInput = document.getElementsByName("volume").length == 0 ? undefined : document.getElementsByName("volume")[0];
     let temperatureInput = document.getElementsByName("temperature").length == 0 ? undefined : document.getElementsByName("temperature")[0];
 
+    // If the device name is not input
     if( deviceSelect.value === "" ) {
         alert("Device must be selected");
         deviceSelect.focus();
         return false;
     }
 
+    // If the volume is not input
     if( volumeInput !== undefined && volumeInput.value === "" ) {
         alert("Volume must be filled out");
         volumeInput.focus();
         return false;
     }
 
+    // If the volume is not within a reasonable range
     if( volumeInput !== undefined && (Number(volumeInput.value) < 0 || Number(volumeInput.value) > 100) ) {
         alert("Volume must be between 0 and 100");
         volumeInput.focus();
         return false;
     }
 
+    // If the temperature is not within a reasonable range
     if( temperatureInput !== undefined && temperatureInput.value === "" ) {
         alert("Temperature must be filled out");
         temperatureInput.focus();
         return false;
     }
 
+    // If the temperature is not within a reasonable range
     if( temperatureInput !== undefined && (Number(temperatureInput.value) < 16 || Number(temperatureInput.value) > 30) ) {
         alert("Temperature must be between 16 and 30");
         temperatureInput.focus();
         return false;
     }
 
+    // If all good, return ture and submit the form
     return( true );
 }
--- topic7/mid-term/routes/main.js
+++ topic7/mid-term/routes/main.js
@@ -1,21 +1,25 @@
 // The main.js file of your application
 module.exports = function (app) {
-    // A route for R1 Home page
+    // A route for R1. This route returns the home page.
     app.get("/", function (req, res) {
         res.render("home.html");
     });
 
-    // A route for R2: About page
+    // A route for R2. This route returns the about page.
     app.get("/aboutPage", function (req, res) {
         res.render("about.html");
     });
 
-    // A route for R3A:
+    // A route for R3A. This route returns an add page allowing to select a device name and correspounding fields to add.
     app.get("/addPage", function (req, res) {
+        // Selects all available devices name and correspounding fields.
+        // Returns a page with available devices name and correspounding fields.
         dbRun("SELECT name, fields FROM devices;").then(value =>res.render("add.ejs", devices=value));
     });
 
-    // A route for R3B
+    // A route for R3B. This route is called by the form in the page rendered by the addPage endpoint.
+    // Given device name and correspounding fields information, inserts the a new observation into the devicesAdded table.
+    // Upon insertion, returns a page with successfully inserted mesage.
     app.post("/add", function (req, res) {
         const {deviceName, ...fields}  = req.body;
         dbRun("INSERT INTO devicesAdded SET ?;", {name: deviceName, fields: JSON.stringify(fields)}).
@@ -23,36 +27,43 @@ module.exports = function (app) {
         ;
     })
 
-    // A route for R4A: Show device page
+    // A route for R4A. This route returns a show page to allow selecting a certain name of device.
+    // Once a device name is selected, showDashboard end point is called to return the result.
     app.get("/showPage", function (req, res) {
+        // Select all possible device names.
         dbRun("SELECT name, fields FROM devices;").then(value =>res.render("show.ejs", {devices: value, selectedDevice: undefined},));
     });
 
-    // Return selected device data
+    // A route for R4B. Tbjs route is called by the form in the show page rendered by showPage end point.
+    // Given a device name, returns any device is found. Otherwise returns a page with nothing is found message.
     app.get("/showDashboard", function (req, res) {
         const deviceName = req.query.deviceName;
+        // Select all devices given a certain device name
         dbRun("SELECT id, name, fields FROM devicesAdded WHERE ?", {name: deviceName}).
         then(value => {
-            // If not found return a message
+            // If nothing is found returns a message
             if (value.length == 0) {
                 return Promise.reject(0);
             }
             else {
-                // Otherwise, return selcted device data
+                // Otherwise, returns the results
                 res.render("show.ejs", {devices: [value[0]], selectedDevice: value}) 
             };
         }).catch(error => res.render("message.ejs", message={title: `${deviceName} is not found`, body: `No ${deviceName} has been inserted before`}))
         ;        
     });
 
-    // A route for R6A:
+    // A route for R6A. This route returns a page with an input box to allow deleting a certain added device by providing a device id.
     app.get("/deletePage", function (req, res) {
         res.render("delete.html");
     });
 
-    // A route for R6B:
+    // A route for R6B. This route is called by the form in the delete page and handles the deletion for device.
+    // If a device is successfully deleted, this route returns a page with successful message.
+    // If nothing is deleted, this route returns a page with nothing deleted message.
     app.post("/delete", function (req, res) {
         const deviceId = req.body.deviceId;
+        // Delete the observation for given device id
         dbRun("DELETE FROM devicesAdded WHERE ?;", {id: deviceId}).
         then(value =>{
             // If nothing is deleted send a message to tell the user the specified device id is not found
@@ -64,7 +75,7 @@ module.exports = function (app) {
         }).catch(value => res.render("message.ejs", message={title: `${deviceId} is not found`, body: `No device with device id ${deviceId} has been inserted before`}));
     });
 
-    // A route for R5A:
+    // A route for R5A. This route returns a page with listed added devices and form to allow updating a certain added device by clicking a device id.
     app.get("/updatePage", async function (req, res) {
         const devices = await dbRun("SELECT name, fields FROM devices;")
         const addedDevices = await dbRun("SELECT id, name FROM devicesAdded;");
@@ -72,18 +83,20 @@ module.exports = function (app) {
         res.render("update.ejs", {devices, addedDevices});
     });
 
-    // A route for R5B:
+    // A route for R5B. This route is called by the form in the update page and handles the update for device.
+    // If a device is successfully updated, this route returns a page with successful message.
+    // If nothing is updated, this route returns a page with nothing updated message.
     app.post("/update", function (req, res) {
         const {deviceId, ...fields}  = req.body;
 
+        // Update the fields for given device id and fields
         dbRun("UPDATE devicesAdded SET fields = ? WHERE id = ?;", [JSON.stringify(fields), Number(deviceId)]).
         then(value =>{
-            console.log(value);
-            // If nothing is deleted send a message to tell the user the specified device id is not found
+            // If nothing is updated send a message to tell the user the specified device id is not found
             if (value.affectedRows === 0) {
                 return Promise.reject(0);
             }
-            // Otherwise, tell the user the specified device id has been deleted
+            // Otherwise, tell the user the specified device id has been updated
             res.send(res.render("message.ejs", message={title: `${deviceId} is updated`, body: `The device with device id ${deviceId} has been updated`}))
         }).catch(value => res.render("message.ejs", message={title: `${deviceId} is not found`, body: `No device with device id ${deviceId} has been inserted before`}));
     });
