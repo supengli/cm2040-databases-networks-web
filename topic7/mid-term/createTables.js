fe66041c0693dea4779da851b91b92027634ca44
Supeng (Soho) Li <superlisohou@gmail.com>
2021-07-19 10:43:23 +0900

    add more data

--- topic7/mid-term/createTables.js
+++ topic7/mid-term/createTables.js
@@ -31,6 +31,19 @@ async function createTables() {
         ('Music player','[{"field_name": "on/off", "field_type": "boolean"}, {"field_name": "volume", "field_type": "number"}]'),
         ('Fridge-freezer', '[{"field_name": "on/off", "field_type": "boolean"}, {"field_name": "open/close", "field_type": "boolean"}, {"field_name": "temperature", "field_type": "number"}]'),
         ('Heating', '[{"field_name": "on/off", "field_type": "boolean"}, {"field_name": "temperature", "field_type": "number"}]'),
+        ('Toaster','[{"field_name": "on/off", "field_type": "boolean"}]'),
+        ('Kettle','[{"field_name": "on/off", "field_type": "boolean"}]'),
+        ('CCTV','[{"field_name": "on/off", "field_type": "boolean"}]'),
+        ('Nintendo-Switch','[{"field_name": "on/off", "field_type": "boolean"}]'),
+        ('Xbox','[{"field_name": "on/off", "field_type": "boolean"}]'),
+        ('Playstation','[{"field_name": "on/off", "field_type": "boolean"}]'),
+        ('Vacuum-cleaner','[{"field_name": "on/off", "field_type": "boolean"}]'),
+        ('Dryer','[{"field_name": "on/off", "field_type": "boolean"}]'),
+        ('Washing-machine','[{"field_name": "on/off", "field_type": "boolean"}]'),
+        ('Wine-cellar','[{"field_name": "on/off", "field_type": "boolean"}]'),
+        ('Camera','[{"field_name": "on/off", "field_type": "boolean"}]'),
+        ('Video-player','[{"field_name": "on/off", "field_type": "boolean"}]'),
+        ('Computer','[{"field_name": "on/off", "field_type": "boolean"}]'),
         ('Cooling', '[{"field_name": "on/off", "field_type": "boolean"}, {"field_name": "temperature", "field_type": "number"}]');`);
 }
 createTables().then(value => process.exit(1)).catch(error => console.log(error))
